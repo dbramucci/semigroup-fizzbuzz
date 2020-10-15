14cd7cefdd004f614fdf25c33e634bb87e1c9e79
Daniel Bramucci <dbramucci@users.noreply.github.com>
2020-10-14 23:44:13 -0500

    Used `asum` to simplify code

--- fizzbuzz.hs
+++ fizzbuzz.hs
@@ -1,9 +1,6 @@
+import Data.Foldable (asum)
 import Data.Maybe (fromMaybe)
 
-
--- | Zips together lists of monoids using mappend
-monoidZipAll :: (Foldable t, Monoid c) => t [c] -> [c]
-monoidZipAll = foldr (zipWith mappend) (repeat mempty)
 
 -- | Allows fizz and buzz list to be easily made
 mults :: Int -> String -> [Maybe String]
@@ -15,11 +12,13 @@ mults n text = cycle $ replicate (n - 1) Nothing ++ [Just text]
 -- If there are no strings, then the 1-based index is used.
 combine :: Foldable t => t [Maybe String] -> [String]
 combine parts = zipWith (flip fromMaybe)
-                    (monoidZipAll parts)
+                    (asum parts)
                   $ map show [1..]
+
 
 fizzbuzz :: [String]
 fizzbuzz = combine [mults 3 "fizz", mults 5 "buzz"]
 
+
 main :: IO()
-main = mapM_ print $ take 100 fizzbuzz
+main = mapM_ putStrLn $ take 100 fizzbuzz
