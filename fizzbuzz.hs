42466cf3f4de3fb81761cc6b5148b66b95561510
Daniel Bramucci <dbramucci@users.noreply.github.com>
2020-10-14 23:14:58 -0500

    Fixed typo + empty list failure on combine.

--- fizzbuzz.hs
+++ fizzbuzz.hs
@@ -7,13 +7,13 @@ mults n text = cycle $ replicate (n - 1) Nothing ++ [Just text]
 -- | Combines the _ _ fizz ... pattern with the _ _ _ _ buzz ... pattern.
 -- specifically, if there are any strings, they get concatenated left to right.
 -- If there are no strings, then the 1-based index is used.
-combine :: foldable t => t [Maybe String] -> [String]
+combine :: Foldable t => t [Maybe String] -> [String]
 combine parts = zipWith (flip fromMaybe)
-                    (foldr1 (zipWith (<>)) parts)
+                    (foldr (zipWith (<>)) (repeat Nothing) parts)
                   $ map show [1..]
 
 fizzbuzz :: [String]
 fizzbuzz = combine [mults 3 "fizz", mults 5 "buzz"]
-              
+
 main :: IO()
 main = mapM_ print $ take 100 fizzbuzz
