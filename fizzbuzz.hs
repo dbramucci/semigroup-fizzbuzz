14cbcb5767037a08552b2d4590ea04102fb1f2bc
Daniel Bramucci <dbramucci@users.noreply.github.com>
2020-10-14 23:43:03 -0500

    Abstracted out `monoidZipAll`

--- fizzbuzz.hs
+++ fizzbuzz.hs
@@ -1,15 +1,21 @@
 import Data.Maybe (fromMaybe)
 
+
+-- | Zips together lists of monoids using mappend
+monoidZipAll :: (Foldable t, Monoid c) => t [c] -> [c]
+monoidZipAll = foldr (zipWith mappend) (repeat mempty)
+
 -- | Allows fizz and buzz list to be easily made
 mults :: Int -> String -> [Maybe String]
 mults n text = cycle $ replicate (n - 1) Nothing ++ [Just text]
+
 
 -- | Combines the _ _ fizz ... pattern with the _ _ _ _ buzz ... pattern.
 -- specifically, if there are any strings, they get concatenated left to right.
 -- If there are no strings, then the 1-based index is used.
 combine :: Foldable t => t [Maybe String] -> [String]
 combine parts = zipWith (flip fromMaybe)
-                    (foldr (zipWith (<>)) (repeat Nothing) parts)
+                    (monoidZipAll parts)
                   $ map show [1..]
 
 fizzbuzz :: [String]
